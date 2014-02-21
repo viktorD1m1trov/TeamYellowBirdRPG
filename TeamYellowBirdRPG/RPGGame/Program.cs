bb08057e53af071a220c3f75399482b3d45de8d2
hackohackob <hackohackob@gmail.com>
2014-02-21 13:33:03 +0200

    test commit

--- TeamYellowBirdRPG/RPGGame/Program.cs
+++ TeamYellowBirdRPG/RPGGame/Program.cs
@@ -12,14 +12,10 @@ namespace RPGGame
             ConsoleClass.PrintBorders();
             Map mymap = new Map("60map.txt");
 
-
-            
-            
-
             mymap.PrintWholeMap();
             MessageBox.Print("Telerik Team Yellow Bird");
             MessageBox.Print("Press Enter to Continue");
-
+            MessageBox.Print("Testing commiting!");
             Console.ReadLine();
             MessageBox.Clear();
             Console.ReadLine();
