ca4569de456550b64c8a790583ec384421251840
miroslavavramov <miroslavavramovster@gmail.com>
2014-02-24 00:24:00 +0200

    abstract class for stationary objects profit and flask

---
+++ TeamYellowBirdRPG/RPGGame/StationaryObjects.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGGame
+{
+    public abstract class StationaryObjects : Coordinates
+    {
+        private Hero h;
+        public string Name{ get; set; }
+
+        StationaryObjects(int xPosition, int yPosition, string Name)
+            : base(xPosition, yPosition)
+        {
+            this.Name = Name;
+        }
+
+        public abstract int Profit(int health);
+
+    }
+}
