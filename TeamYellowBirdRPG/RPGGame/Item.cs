b4ff45253ad8cbb6476ccf59ae027efc6d5941e5
ipobornik <gereg.sith@gmail.com>
2014-02-24 23:51:19 +0200

    Add Item class, Coin, Potion

---
+++ TeamYellowBirdRPG/RPGGame/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGGame
+{
+    public abstract class Item
+    {
+        protected Coordinates position;
+
+        public abstract int bonusTo { get; protected set; } //the bonus that the item will give
+
+        public abstract Coordinates Position { get; set; }
+       
+       
+    }
+}
---
+++ TeamYellowBirdRPG/RPGGame/Potion.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGGame
+{
+    public abstract class Potion : Item
+    {
+        public override int bonusTo
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            protected set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override Coordinates Position
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
